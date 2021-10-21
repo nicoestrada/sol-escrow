b849a669392488be20f132c6c85159eecb9b3d4c
Nico <estradanicolas@gmail.com>
2021-10-21 18:19:24 -0500

    added pda and groundwork fn's

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,17 @@
 //error.rs is for program specific errors
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    ///Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
